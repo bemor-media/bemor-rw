cdf6666ede2c4ae29c1b583f25d0275b8fd310bd
Morgan Spencer <morgan@spencercreative.co>
2020-11-06 15:14:33 -0500

    Update Tailwind

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -3,9 +3,25 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: ['./web/src/**/*.js', './web/src/**/*.css', './web/src/**/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: '#E63946',
+        pale: '#F1FAEE',
+        nile: '#A8DADC',
+        night: '#457B9D',
+        slate: '#1D3557',
+      },
+      margin: {
+        '-wrap': '-5.62vw',
+        wrap: '5.62vw',
+      },
+      padding: {
+        wrap: '5.62vw',
+        'wrap-1/2': '2.81vw',
+      },
+    },
   },
   variants: {},
   plugins: [],
